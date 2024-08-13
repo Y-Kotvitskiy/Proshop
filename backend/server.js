0d768ae603a5317fbfa8f80ae7f9990a2212d33d
Yevgen Kotvitskyi <yevgeniy.kotvitskyi@gmail.com>
2024-08-13 22:17:57 +0300

    add env

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,23 @@
 import express from "express";
-import products from "./data/products.js"
+import products from "./data/products.js";
+import dotenv from "dotenv";
+dotenv.config();
 
-const port = 5000;
+const port = process.env.PORT || 80;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send(`API is running..`)
+app.get("/api", (req, res) => {
+  res.send(`API is running..`);
 });
 
-app.get("/products", (req, res) => {
-    res.json(products)
-  });
-  
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((product) => product._id === req.params.id);
+  res.json(product);
+});
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
