7262dceb2f5526247f4b7ecc685489d1e908c8ee
Y. Kotvizky <ekotvizky@gmail.com>
2024-08-14 17:30:23 +0300

    init mongoDb

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -14,7 +14,7 @@ const Product = ({ product }) => {
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
-        <Card.Text>
+        <Card.Text as="div">
           <Rating value={product.rating} text= {`${product.numReviews} reviews`} />
         </Card.Text>
         <Card.Text as="h3">${product.price}</Card.Text>
