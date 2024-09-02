0dbe58a214ac2a37fcef1a4b470faf9ebdf11b7e
Y. Kotvizky <ekotvizky@gmail.com>
2024-09-02 17:54:36 +0300

    write authUser in userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,21 @@ import User from "../models/userModel.js";
 // @route POST /api/users/login
 // @access Public
 const authUser = asyncHandler(async (req, res) => {
-  res.send("auth user");
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    console.log('Error!')
+    throw new Error("Invalid email or password");
+  }
+  // res.send("auth user");
 });
 
 // @desc Register user
@@ -26,53 +40,52 @@ const logoutUser = asyncHandler(async (req, res) => {
 // @route GET /api/users/profile
 // @access Private
 const getUserProfile = asyncHandler(async (req, res) => {
-    res.send("get user profile");
+  res.send("get user profile");
 });
 
 // @desc Update user profile
 // @route PUT /api/users/profile
 // @access Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-    res.send("update user profile");
+  res.send("update user profile");
 });
 
 // @desc Get users
 // @route Get /api/users
 // @access Private/Admin
 const getUsers = asyncHandler(async (req, res) => {
-    res.send("get users");
+  res.send("get users");
 });
 
 // @desc Get user by id
 // @route Get /api/users/:id
 // @access Private/Admin
 const getUserByID = asyncHandler(async (req, res) => {
-    res.send("get user by id");
+  res.send("get user by id");
 });
-
 
 // @desc Delete user
 // @route Delete /api/users/:id
 // @access Private/Admin
 const deleteUser = asyncHandler(async (req, res) => {
-    res.send("delete user");
+  res.send("delete user");
 });
 
 // @desc Update user
 // @route PUT /api/users/:id
 // @access Private/Admin
 const updateUser = asyncHandler(async (req, res) => {
-    res.send("update user");
+  res.send("update user");
 });
 
 export {
-    authUser,
-    registerUser,
-    logoutUser,
-    getUserProfile,
-    updateUserProfile,
-    getUsers,
-    deleteUser,
-    getUserByID,
-    updateUser,
-}
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserByID,
+  updateUser,
+};
--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,11 @@ connectDB(); // connect to MongoDB
 
 const app = express();
 
+// Body parser middleware
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/api", (req, res) => {
   res.send(`API is running..`);
 });
