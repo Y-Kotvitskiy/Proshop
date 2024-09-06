e6235c4484337001a039c2c9abec5f3af2dddf51
Yevgen Kotvitskyi <yevgeniy.kotvitskyi@gmail.com>
2024-09-06 22:20:36 +0300

    order routes, controller

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -29,6 +30,7 @@ app.get("/api", (req, res) => {
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.use(notFound);
 
