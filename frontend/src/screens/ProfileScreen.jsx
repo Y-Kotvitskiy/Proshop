8f114eca235c95c5f89b5e66db456d1b6c7cc7fc
Y. Kotvizky <ekotvizky@gmail.com>
2024-09-17 18:00:46 +0300

    user profile start

---
+++ frontend/src/screens/ProfileScreen.jsx
@@ -0,0 +1,74 @@
+import { useState, useEffect } from "react";
+import { Table, Form, Button, Row, Col } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { useProfileMutation } from "../slices/usersApiSlice";
+import { setCretentials } from "../slices/authSlice";
+
+const ProfileScreen = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setconfirmPassword] = useState("");
+
+  const dispatch = useDispatch();
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (userInfo) {
+      setName(userInfo.name);
+      setEmail(userInfo.email);
+    }
+  }, [userInfo.name, userInfo.email]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+
+  return (
+    <Row>
+      <Col md={3}>
+        <h2>User Profile</h2>
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="name" className="my-2">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="name"
+              placeholder="Enter Name"
+              value={name}
+              onChange={(e) => setName(e.target.name)}
+            />
+          </Form.Group>
+          <Form.Group controlId="email" className="my-2">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.name)}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="password" className="my-2">
+            <Form.Label>Enter Password</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.name)}
+            />
+          </Form.Group>
+
+        </Form>
+      </Col>
+      <Col md={9}></Col>
+    </Row>
+  );
+};
+
+export default ProfileScreen;
