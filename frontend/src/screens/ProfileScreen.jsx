71b0800728937baa9bcfcf6e85b9ca855c3ebba5
Y. Kotvizky <ekotvizky@gmail.com>
2024-09-18 14:16:31 +0300

    add user profile screen:

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -12,22 +12,40 @@ const ProfileScreen = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [confirmPassword, setconfirmPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   const dispatch = useDispatch();
 
   const { userInfo } = useSelector((state) => state.auth);
+
+  const [updateProfile, { isLoading: loadingUpdateProfile }] =
+    useProfileMutation();
 
   useEffect(() => {
     if (userInfo) {
       setName(userInfo.name);
       setEmail(userInfo.email);
     }
-  }, [userInfo.name, userInfo.email]);
+  }, [userInfo, userInfo.name, userInfo.email]);
 
-  const submitHandler = (e) => {
+  const submitHandler = async (e) => {
     e.preventDefault();
-    console.log("submit");
+    if (password != confirmPassword) {
+      toast.error("Password do not match");
+    } else {
+      try {
+        const res = await updateProfile({
+          _id: userInfo.id,
+          name,
+          email,
+          password,
+        }).unwrap();
+        dispatch(setCretentials(res));
+        toast.success("Profile updated successfully");
+      } catch (err) {
+        toast.error(err?.data?.message || err.error);
+      }
+    }
   };
 
   return (
@@ -41,29 +59,43 @@ const ProfileScreen = () => {
               type="name"
               placeholder="Enter Name"
               value={name}
-              onChange={(e) => setName(e.target.name)}
+              onChange={(e) => setName(e.target.value)}
             />
           </Form.Group>
+
           <Form.Group controlId="email" className="my-2">
             <Form.Label>Email Address</Form.Label>
             <Form.Control
               type="email"
               placeholder="Enter Email"
               value={email}
-              onChange={(e) => setEmail(e.target.name)}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </Form.Group>
 
           <Form.Group controlId="password" className="my-2">
             <Form.Label>Enter Password</Form.Label>
             <Form.Control
-              type="email"
+              type="password"
               placeholder="Enter Password"
               value={password}
-              onChange={(e) => setPassword(e.target.name)}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </Form.Group>
 
+          <Form.Group controlId="confirmpassword" className="my-2">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Confirm Password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </Form.Group>
+          {loadingUpdateProfile && <Loader />}
+          <Button md={9} className="my-2" onClick={submitHandler}>
+            Confirm
+          </Button>
         </Form>
       </Col>
       <Col md={9}></Col>
